5ae5b0ee99810db5a6d91cec21cc39ec47e800b6
PepeOrtega <42347868+JoseOrtegaB@users.noreply.github.com>
2019-01-15 16:00:19 -0600

    Add files via upload
    tipos de datos en JS

---
+++ tiposDatos.js
@@ -0,0 +1,18 @@
+//Tipos de datos en javascript
+//la funcion typeof, nos permite conocer el tipo de dato
+//que hemos registrado
+
+const nombre = "Fernando";
+console.log(typeof(nombre));
+
+const aprendiendoJs= true;
+console.log(typeof(aprendiendoJs));
+
+let valor;
+console.log(typeof(valor));
+valor = 1993;
+console.log(typeof(valor));
+valor ="Hola";
+console.log(typeof(valor));
+valor = false;
+console.log(typeof(valor));
