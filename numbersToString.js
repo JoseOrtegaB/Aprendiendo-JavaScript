48c21873bedb07d001f448f93d62f328a5cf05fe
PepeOrtega <42347868+JoseOrtegaB@users.noreply.github.com>
2019-01-15 15:24:57 -0600

    Add files via upload
    Convertir numeros a Strings

---
+++ numbersToString.js
@@ -0,0 +1,10 @@
+//numeros a strings
+//Para casos en que se necesite saber el numero de caracteres
+//de un numeros
+
+let dato = 8978685493858586;
+
+dato = String(true);
+dato = String(new Date());
+
+console.log(dato.length);
